10dec2e8afdd4e8c89c55e0f0493859eaece7f5c
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-08-07 23:00:11 -0300

    Remoção de Dependências do sample

--- Demo/Delphi/Demo_Delphi.dpr
+++ Demo/Delphi/Demo_Delphi.dpr
@@ -15,6 +15,5 @@ begin
   Application.Initialize;
   eTranslate(ExtractFilePath(ParamStr(0)) + 'translate.json', 'pt-BR');
   Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
